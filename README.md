439976df25e004009920d8240609a2efd969a561
xas <xas@users.noreply.github.com>
2022-04-06 09:36:59 +0200

    feat: initial release

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# centralpackageversions-generator
+# CentralPackageVersions Generator
+
+Dirty code which parse a root folder for all csproj files available :
+
+* update the `path` variable to the root of your solution
+* find the `<PackageReference>` line
+* save it
+* create a new _Packages.props_ file at the root folder
+* create an _ItemGroup_ element
+* add all the `<PackageReference>` found
+* replace `Include` with `Update` attribute
+* define `Version` as an attribute (instead of a child element)
+* save
+* remove the `Version` attribute in the `<PackageReference>` from all the csproj files
+
+You still need to create the _Directory.Build.targets_ file with this content :
+
+```xml
+<Project>
+  <Sdk Name="Microsoft.Build.CentralPackageVersions" Version="2.1.3" />
+</Project>
+```
+
+## TODO
+
+I was not able to set the `Update` attribute **before** the `Version` attribute.  
+I didn't look more than that, I don't know if it's possible or not.
---
+++ src/Xas.CentralPackageVersions.Generator/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Build.Construction;
+
+string path = "";
+var projectFiles = Directory.GetFiles(path, "*.csproj", SearchOption.AllDirectories);
+Dictionary<string, ProjectItemElement> packages = new Dictionary<string, ProjectItemElement>();
+foreach (string project in projectFiles)
+{
+    ProjectRootElement projectRootElement = ProjectRootElement.Open(project);
+    foreach (ProjectItemGroupElement item in projectRootElement.ItemGroups)
+    {
+        foreach (ProjectItemElement element in item.Children.OfType<ProjectItemElement>())
+        {
+            if (element.ElementName == "PackageReference" && !packages.ContainsKey(element.Include))
+            {
+                packages.Add(element.Include, element);
+            }
+        }
+    }
+}
+Console.WriteLine($"Found {packages.Count} packages");
+ProjectRootElement props = ProjectRootElement.Create();
+ProjectItemGroupElement itemGroup = props.AddItemGroup();
+foreach (KeyValuePair<string, ProjectItemElement> outerKvp in packages.OrderBy(x => x.Key))
+{
+    ProjectMetadataElement[] temp = new ProjectMetadataElement[outerKvp.Value.Metadata.Count];
+    outerKvp.Value.Metadata.CopyTo(temp, 0);
+    ProjectItemElement item = itemGroup.AddItem("PackageReference", outerKvp.Key);
+    foreach (ProjectMetadataElement metadata in temp)
+    {
+        item.AddMetadata(metadata.Name, metadata.Value, metadata.ExpressedAsAttribute);
+    }
+    item.Include = null;
+    item.Update = outerKvp.Key;
+}
+props.Save(Path.Combine(path, "Packages.props"));
+
+foreach (string project in projectFiles)
+{
+    ProjectRootElement projectRootElement = ProjectRootElement.Open(project);
+    foreach (ProjectItemGroupElement item in projectRootElement.ItemGroups)
+    {
+        foreach (ProjectItemElement element in item.Children.OfType<ProjectItemElement>())
+        {
+            if (element.ElementName == "PackageReference")
+            {
+                var metadataVersion = element.Metadata.Where(x => x.Name == "Version").FirstOrDefault();
+                if (metadataVersion != null)
+                {
+                    metadataVersion.Value = string.Empty;
+                }
+            }
+        }
+    }
+    projectRootElement.Save();
+}
